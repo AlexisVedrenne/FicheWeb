3b1dba0c8044e9bcd845cfc0890fa7cc2548cece
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2021-09-25 14:05:53 +0200

    Modification du script inscription.js Maintenant le bouton d'enregistrement est désactiver si le mdp de ne correspond pas

--- public/assets/js/inscription.js
+++ public/assets/js/inscription.js
@@ -1,10 +1,11 @@
 var check = function() {
-    if (document.getElementById('registration_form_plainPassword').value ==
-        document.getElementById('confirm_password').value) {
+    if (document.getElementById('registration_form_plainPassword').value == document.getElementById('confirm_password').value) {
         document.getElementById('message').style.color = 'green';
         document.getElementById('message').innerHTML = 'Le mot de passe correspond';
+        document.getElementById('btnEnregistrer').removeAttribute("disabled")
     } else {
         document.getElementById('message').style.color = 'red';
+        document.getElementById('btnEnregistrer').setAttribute("disabled", true)
         document.getElementById('message').innerHTML = 'Le mot de passe ne correspond pas !';
     }
 }
