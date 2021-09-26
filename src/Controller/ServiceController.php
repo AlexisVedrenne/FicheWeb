4e9691d25f3c171370392b2212b138cebc5f3ccc
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2021-09-26 15:39:33 +0200

    Fonctionnement du code de vérification envoyer par mail

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -11,13 +11,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Controller\AppController;
 use App\Form\ValideCodeType;
+use App\Service\Mail;
+use XMLWriter;
 
-    /**
+/**
      * @Route("/service", name="service_")
      */
 class ServiceController extends AbstractController
 {
-
+    
     public function index(): Response
     {
         return $this->render('service/index.html.twig', [
@@ -27,20 +29,52 @@ class ServiceController extends AbstractController
 
 
     /**
-     * @Route("/fireWall",name="fireWall")
+     * @Route("/motDePasseOublier",name="demande")
+     */
+    public function dmdMdp(Request $request){ 
+        
+        if( ($request->request->get('mail')==null)){
+            return $this->render('service/motDePasseOublier/demandeMail.html.twig');
+        }
+        else{
+            $longeur= 8;
+            $code=AppController::codeGen($longeur);
+            // Mail::mdpOublier($request->request->get('mail'),$code);
+            $xml=new XMLWriter();
+            $xml->openUri("varableglobal.xml");
+            $xml->startElement('mdp');
+            $xml->writeElement('code',$code);
+            $xml->endElement();
+            $xml->flush();
+            return $this->redirectToRoute('service_firewall');
+        }
+    }
+
+    /**
+     * @Route("/fireWall",name="firewall")
      */
     public function fireWallMdp(Request $request){
-        $longeur= 8;
-        $code=AppController::codeGen($longeur);
+        $code=utf8_decode(simplexml_load_file("varableglobal.xml")->{"code"});
+        var_dump($code);
+        $longeur=8;
         $form=$this->createForm(ValideCodeType::class);
         $form->handleRequest($request);
         if($form->isSubmitted()&&$form->isValid()){
+            $submitCode="";
+            for ($i = 1; $i <= $longeur; $i++) {
+                $submitCode=$submitCode."".(string)$request->request->get('digit-'.$i);
+            }
+             
+            if($submitCode==$code){
+                return $this->redirectToRoute('service_mdp');
+            }
             
-            return $this->redirectToRoute('service_mdp');
         }
         return $this->render('service/motDePasseOublier/valideCode.html.twig',['form'=>$form->createView(),'code'=>$longeur]);
     }
 
+
+    
     public function motDePasseOublier(UserRepository $repo, int $id,int $code,Request $request, UserPasswordEncoderInterface $passwordEncoder){
         $user=$repo->find($id);
     }
