e89d44a8f086a2fadefff3887bf77a9737e3b9f7
Francis <francis.zhao2000@gmail.com>
2021-09-27 17:29:18 +0200

    Controller uneFiche (id)

--- src/Controller/FicheController.php
+++ src/Controller/FicheController.php
@@ -5,17 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\FicheRepository;
+/**
+ * @Route("/fiche", name="fiche_")
+ */
 class FicheController extends AbstractController
 {
-    /**
-     * @Route("/fiche", name="fiche")
-     */
+
     public function index(): Response
     {
         return $this->render('fiche/index.html.twig', [
             'controller_name' => 'FicheController',
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="uneFiche")
+     * Undocumented function
+     *
+     * @param FicheRepository $repo
+     * @param [type] $id
+     * @return void
+     */
+
+    public function getFiche(FicheRepository $repo, $id){
+        $fiche = $repo->find($id);
+
+        return $this->render('fiche/fiche.html.twig',['fiche'=>$fiche]);
     }
 
     
