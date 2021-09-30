e82daa01eab96bacb7a1567a1a414994373fcffe
akalmous <akalmousikram@gmail.com>
2021-09-30 17:16:14 +0200

    modification des fiches

--- src/Controller/FicheController.php
+++ src/Controller/FicheController.php
@@ -12,6 +12,9 @@ use App\Form\FicheType;
 use App\Entity\Fiche;
 use DateTime;
 use DateTimeInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\Categorie;
 
 /**
 * @Route("/fiche", name="fiche_")
@@ -32,6 +35,7 @@ class FicheController extends AbstractController
      */
     public function addFiche(Request $request, EntityManagerInterface $manager )
     {
+        
         $fiche= new Fiche();
         $form=$this->createForm(FicheType::class,$fiche);
         $form->handleRequest($request);
@@ -47,4 +51,46 @@ class FicheController extends AbstractController
         return $this->render('fiche/ajouter.html.twig',['form'=>$form->createView()]);
 
     }
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * c'est une fonction qui permet de modifier une fiche (nom/catégorie)
+     */
+    public function editFiche(Request $request, $id){
+        $fiche=$this->getDoctrine()->getRepository(Fiche::class);
+        $fiche=$fiche->find($id);
+    
+        //message d'erruer au cas ou l'id n'existe pas
+    if (!$fiche) {
+        throw $this->createNotFoundException(
+            "il n'ya pas de fiche avec cet id" . $id);
+    
+    }
+            //affichage du formulaire
+    $form=$this->createFormBuilder($fiche)
+            ->add('nom',TextType::class)
+            ->add('laCategorie',EntityType::class,[
+                'class'=>Categorie::class,
+                'choice_label'=>'nom',
+                'expanded'=>false,
+                'multiple'=>false,
+
+            ])  
+
+            ->getForm();
+            $form->handleRequest($request);
+            // récupération des donnés et modifcation
+            if ($form->isSubmitted()) {
+                $em=$this->getDoctrine()->getManager();
+                $fiche=$form->getData();
+                $em->flush();
+            //redirection vers la page d'accueil (juste temporaire)
+                return $this->redirectToRoute('home');
+                # code...
+            }
+        
+            return $this->render('fiche/modifier.html.twig',array('form'=> $form->createView()));
+           
+        ;
+
+}
 }
