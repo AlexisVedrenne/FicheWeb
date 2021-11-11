ca73420403c6705aad5849a0a5943d0216c99125
Alexis <alexisvedrenne482@gmail.com>
2021-11-11 23:48:18 +0100

    Mise à jour du js pour le changement de mot de passe

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,13 +6,14 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('password');
+            ->add('password',PasswordType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
