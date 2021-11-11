ea71837b8a29e3450b69bbd8ddc075e4d4793993
Alexis <alexisvedrenne482@gmail.com>
2021-11-12 00:56:46 +0100

    Création du fichier pour lancer le site en local

--- src/Service/Mail.php
+++ src/Service/Mail.php
@@ -74,7 +74,7 @@ class Mail
           'TextPart' => "",
           'HTMLPart' => "<h1>Merci pour votre inscription " . $user->getPseudo() . " !</h1>
           <p>Si vous recevez cette email c'est que votre compte à parfaitement été créer sur notre platforme.<br> Nous vous invitons à vous connecter pour pouvoir acceder à plus de contenue sur notre site.
-          En vous connectant <a href='http://local.ficheweb/connexion'>ici</a>
+          En vous connectant <a href='https://localfiche.loca.lt/connexion'>ici</a>
           </p>
           <h4>Merci à vous, l'équipe FicheWeb</h4>
           <h6>Veuillez ne pas répondre à ce mail générer automatiquement</h6>",
---
+++ start.bat
@@ -0,0 +1,5 @@
+start cmd /k symfony server:start
+powershell -command "Start-Sleep -s 5"
+lt --port 8000 --subdomain localfiche -o
+powershell -command "Start-Sleep -s 5"
+
